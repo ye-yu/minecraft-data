394c671facd28c3ad57f46d388a3a753b5fb8f64
ye-yu <rafolwen98@gmail.com>
2020-09-04 19:30:33 +0800

    Use camera to raytrace visible entities

--- src/main/kotlin/fp/yeyu/mcdata/AttributeUtil.kt
+++ src/main/kotlin/fp/yeyu/mcdata/AttributeUtil.kt
@@ -4,12 +4,17 @@ import net.fabricmc.api.EnvType
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.MinecraftClient
 import net.minecraft.entity.Entity
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.ItemStack
 import net.minecraft.network.PacketByteBuf
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.hit.HitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.RayTraceContext
+import net.minecraft.world.World
 import java.util.stream.IntStream
 import kotlin.streams.asSequence
 
@@ -40,7 +45,11 @@ object AttributeUtil {
 
     fun getHotBarCursor(player: PlayerEntity): String = "cursor: ${player.inventory.selectedSlot}"
     fun getVisibleMob(player: PlayerEntity): String {
-        return "mobs: ?"
+        val world = if (isClient) MinecraftClient.getInstance().world ?: return "mobs: ?" else player.world
+        val distance = 100
+        val box = Box(player.blockPos.lowerCorner(distance), player.blockPos.upperCorner(distance))
+        val entities = world.getEntitiesByClass(LivingEntity::class.java, box) { true }
+        return "mobs: " + entities.filter{ isEntityVisible(player, it) }.joinToString { it.toString() }
     }
 
     fun getInventory(player: PlayerEntity): String {
@@ -58,4 +67,29 @@ object AttributeUtil {
     fun getTimeStamp(): String = "${System.currentTimeMillis()}"
     fun getEndStamp(): String = "====="
     fun getUUID(player: PlayerEntity): String = "uuid: ${player.uuidAsString}"
+
+    private fun isEntityVisible(player: PlayerEntity, target: Entity): Boolean {
+        val camera = if (isClient) MinecraftClient.getInstance().cameraEntity ?: return false else (player as ServerPlayerEntity).cameraEntity
+        return isEntityVisible(target, target.pos, camera.pos)
+    }
+
+    private fun isEntityVisible(sourceEntity: Entity, sourcePos: Vec3d, targetPos: Vec3d): Boolean {
+        val world = sourceEntity.world
+
+        val rayTraceContext = RayTraceContext(sourcePos, targetPos, RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.NONE, sourceEntity)
+        return world.rayTrace(rayTraceContext).let {
+            when(it.type) {
+                HitResult.Type.BLOCK -> false
+                else -> true
+            }
+        }
+    }
+
+    private fun isOfEntityAt(world: World, position: BlockPos, target: Entity): Boolean {
+        val entitiesByClass = world.getEntitiesByClass(target::class.java, Box(position), null)
+        return entitiesByClass.any { it == target }
+    }
+
+    private fun BlockPos.lowerCorner(distance: Int): BlockPos = BlockPos(this.x - distance, 0, this.z - distance)
+    private fun BlockPos.upperCorner(distance: Int): BlockPos = BlockPos(this.x + distance, 256, this.z + distance)
 }
